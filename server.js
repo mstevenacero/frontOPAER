37cdd8d3169beaf25d823e7262f6f756ec96d75e
Michael Acero <michaelacero@MacBook-Air-de-Michael.local>
2022-05-27 13:59:37 -0500

    changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front-opera'));
+app.use(express.static('./dist/fron-opera'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front-opera/'}),
+    res.sendFile('index.html', {root: 'dist/fron-opera/'}),
 );
 //hol
 // Start the app by listening on the default Heroku port
